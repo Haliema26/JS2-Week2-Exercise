81594826f92d0536dedcd31115622756af908894
Haliema26 <77581177+Haliema26@users.noreply.github.com>
2021-07-24 15:31:21 +0100

    answers tasks1,2,3

--- A-dom-manipulation/exercise.js
+++ A-dom-manipulation/exercise.js
@@ -15,23 +15,31 @@ Write JavaScript below that logs:
     --> should log a list of nodes with a length of 3
 
 */
-
-
+//Answers:
+document.querySelectorAll("p");
+document.getElementsByClassName("site-header");
+document.getElementById("jumbotron-text");
+document.querySelectorAll("p.lead");
 /*
 Task 2
 ======
 
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
-
-
+var button = document.getElementById("alertBtn");
+button.onclick = function () {
+  alert("Thanks for visiting Bikes for Refugees");
+};
 /*
 Task 3
 =======
 
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
-
+let backgroundClr = document.querySelector("#bgrChangeBtn");
+backgroundClr.addEventListener("click", () => {
+  document.body.style.background = "red";
+});
 
 /*
 Task 4
@@ -39,8 +47,10 @@ Task 4
 
 When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
 */
-
-
+var addTxt = "Read more below";
+function Text() {
+  document.getElementById("addTextBtn").innerHTML = addTxt;
+}
 
 /*
 Task 5
